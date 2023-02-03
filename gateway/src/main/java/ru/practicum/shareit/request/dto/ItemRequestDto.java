93f6380879f747dff783f00610bf161248e45fa5
Андрей Скоробогатов <ufo1og@yandex.ru>
2023-02-03 11:36:41 +0500

    feat: Подготовить проект к сборке через docker-compose.

---
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.request.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemShortDto;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemRequestDto {
+    private Long id;
+    @NotBlank
+    private String description;
+    private LocalDateTime created;
+    private List<ItemShortDto> items;
+}
